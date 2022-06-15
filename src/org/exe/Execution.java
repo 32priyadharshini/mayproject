4e15ba05031aba2b60393a31b000e2e541b6d4c9
pradeep rajan <priyadharshini121998@gmail.com>
2022-06-15 22:15:22 +0530

    second day push

--- src/org/exe/Execution.java
+++ src/org/exe/Execution.java
@@ -1,7 +1,13 @@
 package org.exe;
 
 public class Execution {
-	public static void main(String[] args) {
+	private void empId() {
+		System.out.println("empId:2345");
+	}
+	public static <empId> void main(String[] args) {
+		Execution e=new Execution();
+		e.empId();
+		
 		System.out.println("GITHUB");
 	}
 
