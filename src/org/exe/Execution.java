05cfa20af3de1930e548a0d07648566dbe06dd7c
pradeep rajan <priyadharshini121998@gmail.com>
2022-06-16 12:13:01 +0530

    third day push priya

--- src/org/exe/Execution.java
+++ src/org/exe/Execution.java
@@ -1,7 +1,12 @@
 package org.exe;
 
 public class Execution {
+	private void empName() {
+		System.out.println("priya");
+	}
 	public static void main(String[] args) {
+		Execution e = new Execution();
+		e.empName();
 		System.out.println("GITHUB");
 	}
 
